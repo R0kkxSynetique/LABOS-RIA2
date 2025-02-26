7e21ac9e1b83f890c68cd6e000982373107efd06
Benjamin Fontana <ben192003@gmail.com>
2025-02-26 16:06:10 +0100

    feat: implement component to render items

---
+++ data-binding-vue/src/components/DataBinding.vue
@@ -0,0 +1,24 @@
+<script setup>
+const props = defineProps({
+    name: {
+        type: String,
+        default: "John Doe",
+    },
+    items: {
+        type: Array,
+        default: () => ["Item 1", "Item 2", "Item 3", "Item 4"],
+    },
+});
+</script>
+
+<template>
+    <h1>Vue Data Binding</h1>
+    <span id="name-container">
+        <p>{{ name }}</p>
+    </span>
+    <ul id="items-container">
+        <li v-for="item in items" :key="item">
+            {{ item }}
+        </li>
+    </ul>
+</template>
