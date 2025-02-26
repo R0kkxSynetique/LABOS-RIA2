4aafd21feec1389e9abbee3611c9c50f2851bdc1
Benjamin Fontana <ben192003@gmail.com>
2025-02-26 16:05:35 +0100

    feat: implement initial app state

---
+++ data-binding-vue/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import DataBinding from "@/components/DataBinding.vue";
+
+const items = ["Item 1", "Item 2", "Item 3", "Item 4"]
+const name = "John Doe"
+
+</script>
+
+<template>
+    <DataBinding :name="name" :items='items'/>
+</template>
